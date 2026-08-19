194fe574ee04b7539efc0bd51d08ceec89191a7e
Raphael R. S. Cerqueira <raphael.santana@percorre.org.br>
2026-08-19 16:54:00 -0300

    fix: correção import font Inter

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -21,6 +21,10 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <script src="https://unpkg.com/@phosphor-icons/web"></script>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     </head>
 
     <body class="h-full antialiased text-gray-900 transition-colors duration-300 bg-slate-50 dark:bg-gray-900 dark:text-gray-100">
