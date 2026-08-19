9884f0b69c561cae2c67759193ff1787ba838593
Raphael R. S. Cerqueira <raphael.santana@percorre.org.br>
2026-08-19 16:06:28 -0300

    feat: registro de logs nas funcionalidades de comunicação

--- app/Modules/Comunicacao/Domain/Models/Automacao.php
+++ app/Modules/Comunicacao/Domain/Models/Automacao.php
@@ -3,9 +3,12 @@
 namespace App\Modules\Comunicacao\Domain\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\RegistraAuditoria;
 
 class Automacao extends Model
 {
+    
+    use RegistraAuditoria;
     protected $table = 'automacoes';
     protected $guarded = [];
     protected $casts = [
--- app/Modules/Comunicacao/Domain/Models/EmailTemplate.php
+++ app/Modules/Comunicacao/Domain/Models/EmailTemplate.php
@@ -3,9 +3,12 @@
 namespace App\Modules\Comunicacao\Domain\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\RegistraAuditoria;
 
 class EmailTemplate extends Model
 {
+    
+    use RegistraAuditoria;
     protected $table = 'email_templates';
     protected $guarded = [];
 }
